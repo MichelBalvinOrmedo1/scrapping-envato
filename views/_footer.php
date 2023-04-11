b6190d5bf8a55b9a26e0ba9531cdd3cc84cb6510
osmarthvargas100 <osmarthvargas2003@gmail.com>
2023-04-11 17:35:14 -0500

    ESTILOS Y IMAGEN Y LOGOS FOOTER ACTUALIZADO PARTE 3

--- views/_footer.php
+++ views/_footer.php
@@ -5,26 +5,22 @@
     <!--Grid row-->
     <div class="row">
       <!--Grid column-->
-      <div class="col-lg-6 col-md-12 mb-4 mb-md-0 ">
-        <h5 class="text-uppercase">Footer text</h5>
+      <div class="col-lg-6 col-md-12 mb-4 mb-md-0 texto">
+        <h5 class="text-uppercase">DISEÑO TOTAL</h5>
 
         <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
-          molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae
-          aliquam voluptatem veniam, est atque cumque eum delectus sint!
+        Al usar este sitio,usted acepta nuestros términos de uso y
+        nuestra política de privacidad.
+        Es una marca registrada de la Fundación total, Inc.
         </p>
       </div>
       <!--Grid column-->
 
       <!--Grid column-->
-      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-        <h5 class="text-uppercase">Footer text</h5>
-
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
-          molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae
-          aliquam voluptatem veniam, est atque cumque eum delectus sint!
-        </p>
+      <div class="col-lg-6 col-md-12 mb-4 mb-md-0 ">
+        <img class="img-fb" src="/img/FB.png" alt="" >
+        <img class="img-twiter" src="/img/twuiter.png" alt="">
+        <img class="img-instagram" src="/img/instagram.png" alt="">
       </div>
       <!--Grid column-->
     </div>
@@ -35,7 +31,7 @@
   <!-- Copyright -->
   <div class="text-center p-2 tamaño" style="background-color: #FFFFFF;">
     © 2020 Copyright:
-    <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    <a class="text-dark" href="##">Copyright © 2023 Diseño Total | Aviso Legal | Política de Privacidad | Política de Cookies | Condiciones</a>
   </div>
   <!-- Copyright -->
 </footer>

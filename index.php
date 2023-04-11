2d4d79db02d32f4af359917614e5071bb103a770
osmarthvargas100 <osmarthvargas2003@gmail.com>
2023-04-11 14:38:32 -0500

    CREACION DE INDEX EN PROCESO PARTE 1

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
-<?php include_once('views/_head.html')?>
-<?php include_once('views/_header.php')?>
+<?php include_once('views/_head.php')?>
+
 <main class="container mx-auto">
-    <div class="d-flex flex-column align-items-center">
-      <h2 class="my-4">Buscador de productos</h2>
+    <div class="d-flex flex-column align-items-center subirlo">
+      <img class ="img" src="/img/captura.png" alt="Descripción de la imagen" />
+      
       <form class="d-flex col-12">
-        <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Buscar">
+        <input class="form-control me-2 input" type="search" placeholder="Plantillas o Recursos..." aria-label="Buscar">
         <button class="btn btn-success" type="submit">Buscar</button>
+
+        
       </form>
     </div>
+
 </main>
 
-<?php include_once('views/_footer.html')?>
+
---
+++ views/_footer.php
@@ -0,0 +1,41 @@
+<?php include_once('views/_head.php')?>
+<footer class="bg text-center text-lg-start footer color">
+  <!-- Grid container -->
+  <div class="container p-4" style="background-color: #547C64;">
+    <!--Grid row-->
+    <div class="row">
+      <!--Grid column-->
+      <div class="col-lg-6 col-md-12 mb-4 mb-md-0 ">
+        <h5 class="text-uppercase">Footer text</h5>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
+          molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae
+          aliquam voluptatem veniam, est atque cumque eum delectus sint!
+        </p>
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Footer text</h5>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
+          molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae
+          aliquam voluptatem veniam, est atque cumque eum delectus sint!
+        </p>
+      </div>
+      <!--Grid column-->
+    </div>
+    <!--Grid row-->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-2 tamaño" style="background-color: #FFFFFF;">
+    © 2020 Copyright:
+    <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
